a36592322618cbfc3a23106caeabf66d33d6a0e3
fcody <fcody@fcodys-MacBook.local>
2021-11-22 05:16:48 +0300

    config, auto scanning files, projs, counting, draw projects

--- analyse.py
+++ analyse.py
@@ -1,4 +1,4 @@
-from os import close, read
+from os import access, close, read
 from tkinter import *
 import sys
 import time
@@ -7,81 +7,173 @@ import os
 from tkinter import filedialog
 from tkinter import ttk
 
+class Active(object):
+    def __init__(self, time, duration):
+        self.time = 0
+        self.duration = 0
+
+class Passive(object):
+    def __init__(self, time, duration):
+        self.time = 0
+        self.duration = 0
+
+class Project(object):
+    def __init__(self, name, rate):
+        self.name = "projectName"
+        self.rate = 1000
+        self.active = []
+        self.passive = []
+        self.durationActive = 0
+        self.durationPassive = 0
+        self.durationAll = 0
+
+    def printAllElems(self):
+        print(self.name)
+        print(self.rate)
+        print(self.active)
+        print(self.passive)
+
+    def getTimeActive(self):
+        return self.timeActive
+
+    def getTimePassive(self):
+        return self.timePassive
+
+    def getProjectName(self):
+        return self.name 
+
+def configFileRead():
+    if os.path.isfile(configFileName):
+        configFile = open(configFileName, "r")
+    else:
+        return
+    filePath = configFile.readline()
+    if not filePath:
+        return 
+    return filePath
 
 def getFolderPath(self):
     folder_selected = filedialog.askdirectory()
     folderPath.set(folder_selected)
-    folder = folderPath.get()
-    files = os.listdir(folder)
-    print(files)
+    filePath = folderPath.get()
+    filePath += "/"
+    configFile = open(configFileName, "w")
+    configFile.write(filePath)
 
+def durationSumm(proj):
+    res = 0
+    k = 0
+    while k < len(proj.active):
+        res += proj.active[k].duration
+        k+=1
+    proj.durationActive = res
+    res = 0
+    k = 0
+    while k < len(proj.passive):
+        res += proj.passive[k].duration
+        k+=1
+    proj.durationPassive = res
+    proj.durationAll = proj.durationActive + proj.durationPassive
+    
+
+def drawProjects(self):
+    j = 0
+    while j < len(projects):
+        durationSumm(projects[j])
+        l = Label(bg='yellow', text=(projects[j].name + " " + str((projects[j].durationAll)/60)), justify='left')
+        l.place(x = 5, y = 45 + j * 30, width = (projects[j].durationAll)/60 * 5, height = 30)
+        j+=1
+
+
+def spaceCatch(currentLine):
+    currentLine = str(currentLine)
+    index = currentLine.find(' ')
+    return index
+
+def contains(list, value):
+    c = 0
+    while c < len(list):
+        if list[c].name == value:
+            return c
+        c+=1
+    return -1
+
+def appendActivePassive(elem,currentLineTime, currentLineDuration,currentLineActiveness):
+    if(currentLineActiveness):
+                active = Active(currentLineTime, currentLineDuration)
+                active.duration = currentLineDuration
+                active.time = currentLineTime
+                elem.active.append(active)
+    if not (currentLineActiveness):
+        passive = Passive(currentLineTime, currentLineDuration)
+        passive.duration = currentLineDuration
+        passive.time = currentLineTime
+        elem.passive.append(passive)
+
+def lineAnalyse(currentFile):
+    currentLine = currentFile.readline()
+    if not currentLine or len(currentLine) < 1:
+        return -1
+    else:
+        currentLineSplitted = currentLine.split(' ')
+        currentLineProject = currentLineSplitted[0]
+        currentLineTime = int(currentLineSplitted[1])
+        currentLineDuration = int(currentLineSplitted[2])
+        currentLineActiveness = (currentLineSplitted[3])
+        currentLineActiveness = currentLineActiveness[:-2]
+        if(currentLineActiveness == "active"):
+            currentLineActiveness = 1
+        else:
+            currentLineActiveness = 0
+        index = contains(projects,currentLineProject)
+        if index == -1:
+            newProj = Project(currentLineProject, 1000)
+            newProj.name = currentLineProject
+            appendActivePassive(newProj,currentLineTime,currentLineDuration,currentLineActiveness)
+            projects.append(newProj)
+        else:
+            appendActivePassive(projects[index],currentLineTime,currentLineDuration,currentLineActiveness)
+        return 1
+
+def readFile(currentFileName):
+    print(">>> scanning " + currentFileName)
+    currentFile = open(filePath + currentFileName, "r")
+    while 1:
+        res = lineAnalyse(currentFile)
+        if res < 0:
+            currentFile.close
+            return
+
+def readFiles(files):
+    i = 0
+    while i < len(files):
+        readFile(files[i])
+        i+=1
+    j = 0
+
+projects = []
+configFileName = "config.txt"
 
 gui = Tk()
+gui.title("Resolve Counter Analyse")
+gui.geometry("1900x1000")
+gui.resizable(0,0)
+
 folderPath = StringVar()
-panelFrame = Frame(gui, height = 50, bg = 'gray')
-textFrame = Frame(gui, height = 340, width = 600)
 
-panelFrame.pack(side = 'top', fill = 'x')
-textFrame.pack(side = 'bottom', fill = 'both', expand = 1)
+loadBtn = Button(gui, text="Open directory")
+projBtn = Button(gui, text = 'Projects')
+quitBtn = Button(gui, text = 'n/a')
 
-loadBtn = Button(panelFrame, text="Open directory")
-saveBtn = Button(panelFrame, text = 'n/a')
-quitBtn = Button(panelFrame, text = 'n/a')
+loadBtn.place(x = 5, y = 5, width = 130, height = 30)
+projBtn.place(x = 135, y = 5, width = 65, height = 30)
+quitBtn.place(x = 200, y = 5, width = 40, height = 30)
 
-loadBtn.place(x = 10, y = 10, width = 150, height = 30)
-saveBtn.place(x = 150, y = 10, width = 40, height = 30)
-quitBtn.place(x = 190, y = 10, width = 40, height = 30)
+filePath = configFileRead()
+if(filePath):
+    files = os.listdir(filePath)
+    readFiles(files)
 
 loadBtn.bind("<Button-1>",getFolderPath)
+projBtn.bind("<Button-1>",drawProjects)
 gui.mainloop()
-
-# filePath = "./output/"
-# fileExtension = ".txt"
-# print('file name without extension:')
-# fileOnlyName = str(input())
-# fileName = filePath + fileOnlyName + fileExtension
-# durationActive = int(0)
-# print(fileName)
-# print('proj name:')
-# project = str(input())
-# print('rate:')
-# Rate = int(input())
-# print('Project: ' + project)
-
-# def spaceCatch(currentLine):
-#     currentLine = str(currentLine)
-#     index = currentLine.find(' ')
-#     return index
-
-# def lineAnalyse(type):
-#     currentLine = file.readline()
-#     if not currentLine:
-#         return -1
-#     currentLineSplitted = currentLine.split(' ')
-#     currentLineProject = currentLineSplitted[0]
-#     currentLineTime = int(currentLineSplitted[1])
-#     currentLineDuration = int(currentLineSplitted[2])
-#     currentLineActiveness = (currentLineSplitted[3])
-#     if (currentLineActiveness == type and currentLineProject == project):
-#         return currentLineDuration
-#     return 0
-
-
-# file = open(fileName, "r")
-# sum_active = 0
-# sum_not_active = 0
-# while 1:
-#     res_active = lineAnalyse('active\n')
-#     if res_active == -1:
-#         break
-#     sum_active += res_active
-#     hours_active = float(sum_active) / 3600
-# file.close()
-# file = open(fileName, "r")
-# while 1:
-#     res_not_active = lineAnalyse('not_active\n')
-#     if res_not_active == -1:
-#         break
-#     sum_not_active += res_not_active
-#     hours_not_active = float(sum_not_active) / 3600
-# print(project + ' hoursActive = ' + str(hours_active) + ' hourRateActive = ' + str(int(Rate/hours_active)) + ' hoursTotal = ' + str(hours_active + hours_not_active) + ' hourRateTotal = ' + str(int(Rate/(hours_active + hours_not_active))))
